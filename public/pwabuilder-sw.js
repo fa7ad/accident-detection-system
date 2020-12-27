1f4b3319a2f52f7d26ad99b64ffc9cba8804f39b
Fahad Hossain <set.name.fahad@gmail.com>
2020-12-28 02:23:27 +0600

    fix: cache first sw

--- public/pwabuilder-sw.js
+++ public/pwabuilder-sw.js
@@ -1,19 +1,20 @@
+/* eslint-disable prefer-regex-literals */
 /* eslint-env serviceworker */
 /* global workbox */
 
-const CACHE = "pwabuilder-precache";
+const CACHE = 'pwabuilder-precache'
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js')
 
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
+self.addEventListener('message', event => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting()
   }
-});
+})
 
 workbox.routing.registerRoute(
-  /\/*/,
-  new workbox.strategies.NetworkFirst({
+  new RegExp('/*'),
+  new workbox.strategies.CacheFirst({
     cacheName: CACHE
   })
-);
+)
