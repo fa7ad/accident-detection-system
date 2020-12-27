f4accebc12daec4bf44949ef9c5130015777e9f4
Fahad Hossain <set.name.fahad@gmail.com>
2020-12-28 02:28:04 +0600

    fix: switch to CRA SW

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { HashRouter, BrowserRouter } from 'react-router-dom'
 import Routes from './Routes'
 
 import reportWebVitals from './reportWebVitals'
-// import * as serviceWorkerRegistration from './serviceWorkerRegistration'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 
 import './index.css'
 
@@ -23,7 +23,7 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-// serviceWorkerRegistration.unregister()
+serviceWorkerRegistration.register()
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
