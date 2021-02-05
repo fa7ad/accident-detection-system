8816c5fc0fbd1b2b75a325c8ce6ac047dffaa95b
Fahad Hossain <set.name.fahad@gmail.com>
2021-02-06 01:21:04 +0600

    fix: fix suspense error

--- src/utils/createSuspendedComponent.js
+++ src/utils/createSuspendedComponent.js
@@ -2,10 +2,16 @@ const { Suspense } = require('react')
 
 export const getLoadingFallback = () => <p>Loading...</p>
 
-export const _genSusFback = fallback => Component => props => {
-  ;<Suspense fallback={fallback}>
-    <Component {...props} />
-  </Suspense>
+export const _genSusFback = fallback => Component => {
+  const SuspendedComponent = props => (
+    <Suspense fallback={fallback}>
+      <Component {...props} />
+    </Suspense>
+  )
+
+  SuspendedComponent.displayName = Component?.displayName ?? 'SuspendedComponent'
+
+  return SuspendedComponent
 }
 
 export const createSuspendedComponent = _genSusFback(getLoadingFallback())
